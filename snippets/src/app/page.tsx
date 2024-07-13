8e25531eb48cddef796ccfdbae44eb3eb79c3f82
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-13 08:36:21 +0430

    adds snippets id and title

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,18 +1,29 @@
 import { db } from "@/db"
+import Link from "next/link"
 
 export default async function Home() {
   const snippets = await db.snippet.findMany()
   const renderedSnippets = snippets.map((snippet) => {
     return (
-      <div key={snippet.id}>
-        {snippet.title}
-      </div>
+      <Link
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+        key={snippet.id}>
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
     )
   })
   
   return (
     <div>
-     {renderedSnippets}
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href='/snippets/new' className="border p-2 rounded">NEW</Link>
+      </div>
+      <div className="flex flex-col gap-2">
+       {renderedSnippets}
+      </div>
     </div>
   )
 }
