ba0d6317f0f9e2349f6df5088d556187dc43d1a2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-13 07:59:12 +0430

    adds not found page

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+
+const SnippetNotFound = () => {
+  return (
+      <div>
+          <h1 className="text-xl bold">
+              Sorry, but we couldn\'t find that particular snippet
+          </h1>
+    </div>
+  )
+}
+
+export default SnippetNotFound
