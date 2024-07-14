020ec17657cc5b51e51b06eed1e5ac54df982e95
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-14 07:17:04 +0430

    edit and delete

---
+++ snippets/src/actions/index.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export async function editSnippet(id: number, code: string) {
+    await db.snippet.update({
+        where: { id },
+        data: { code }
+    });
+    redirect(`/snippets/${id}`)
+}
+
+
+export async function deleteSnippet(id:number) {
+    await db.snippet.delete({
+        where: {id}
+    })
+    redirect('/')
+}
