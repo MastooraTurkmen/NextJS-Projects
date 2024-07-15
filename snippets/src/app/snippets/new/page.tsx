9399fb29f40de9830988d5f8a73db2bb11137a4f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-15 06:42:48 +0430

    snippet create page

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,30 +1,13 @@
-import { db } from "@/db"
-import { redirect } from "next/navigation";
+'use client'
+
+import * as actions from "@/actions"
+import { useFormState } from "react-dom"
 
 const SnippetCreatePage = () => {
-    async function createSnippet(formData: FormData) {
-        // This needs to be a server action!
-        'use server';
-
-        // Check the user's inputs and make sure they're valid
-        const title = formData.get('title') as string
-        const code = formData.get('code') as string
-
-        // Create a new record in the database
-        const snippet = await db.snippet.create({
-            data: {
-                title,
-                code
-            }
-        })
-        console.log(snippet);
-        
-        // Redirect the user back to the root route
-        redirect('/')
-    }
+    const [formState, action] = useFormState(actions.createSnippet, {message: ''})
 
     return (
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className="font-bold m-3">Create a Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
@@ -35,6 +18,13 @@ const SnippetCreatePage = () => {
                     <label className="w-12" htmlFor="code">Code</label>
                     <textarea name="code" id="code" className="border rounded p-2 w-full" />
                 </div>
+
+                {
+                    formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-200">
+                        {formState.message}
+                    </div> : null
+                }
+                
                 <button type="submit" className="rounded p-2 text-black bg-blue-200">Create</button>
             </div>
       </form>
