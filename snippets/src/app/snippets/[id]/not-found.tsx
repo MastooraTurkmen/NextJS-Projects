5266a33088980524c41a1284861bd062907158a1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-15 20:01:04 +0430

    set up

--- snippets/src/app/snippets/[id]/not-found.tsx
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -3,7 +3,7 @@ const SnippetNotFound = () => {
   return (
       <div>
           <h1 className="text-xl bold">
-              Sorry, but we couldn\'t find that particular snippet
+              Sorry, but we could not find that particular snippet
           </h1>
     </div>
   )
