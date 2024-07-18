dd6ef13876e4658149510916fbeae2a03ccee425
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-18 07:30:02 +0430

    user profile

--- discuss-app/src/app/page.tsx
+++ discuss-app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/react";
 import * as actions from '@/actions';
 import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
@@ -15,6 +16,7 @@ export default async function Home() {
       {
         session?.user ? <div>{JSON.stringify(session.user)}</div>: <div>Signed Out</div>
       }
+      <Profile />
     </div>
   )
 }
