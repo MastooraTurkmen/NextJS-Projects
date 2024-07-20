c72de9d9ee2151f09af6178ce94a47d90a632410
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-20 21:37:07 +0430

    set up

---
+++ discuss-app/src/components/topics/topic-create-form.tsx
@@ -0,0 +1,34 @@
+import {
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger, 
+    PopoverContent
+} from '@nextui-org/react'
+
+const TopicCreateForm = () => {
+    return (
+        <Popover placement='left'>
+            <PopoverTrigger>
+                <Button color='primary'>Create a Topic</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form>
+                    <div className="flex flex-col gap-4 p-4 w-80">
+                        <h3 className="text-lg">Create a Topic</h3>
+                        <Input label='Name' labelPlacement='outside' placeholder='Name' />
+                        <Textarea
+                            label="Description"
+                            labelPlacement='outside'
+                            placeholder='Describe your topic'
+                        />
+                        <Button type='submit'>Submit</Button>
+                    </div>
+                </form>
+            </PopoverContent>
+      </Popover>
+  )
+}
+
+export default TopicCreateForm
