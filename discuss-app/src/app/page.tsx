bd6ac9622102c278ed3828379182d06a12ae50a4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-20 21:37:35 +0430

    import topic create form page

--- discuss-app/src/app/page.tsx
+++ discuss-app/src/app/page.tsx
@@ -1,5 +1,14 @@
+import TopicCreateForm from "@/components/topics/topic-create-form"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
   )
 }
