2f7735b13c5fec4c6fa4062276d558cd779e6d34
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-21 20:36:04 +0430

    receiving data

--- discuss-app/src/components/topics/topic-create-form.tsx
+++ discuss-app/src/components/topics/topic-create-form.tsx
@@ -17,8 +17,9 @@ const TopicCreateForm = () => {
                 <form>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Topic</h3>
-                        <Input label='Name' labelPlacement='outside' placeholder='Name' />
+                        <Input name='name' label='Name' labelPlacement='outside' placeholder='Name' />
                         <Textarea
+                            name='description'
                             label="Description"
                             labelPlacement='outside'
                             placeholder='Describe your topic'
