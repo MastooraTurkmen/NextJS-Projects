d1deadc92b7c4256ff5e2bbd3b62692632998e19
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-21 20:36:01 +0430

    receiving data

--- discuss-app/src/actions/create-topic.ts
+++ discuss-app/src/actions/create-topic.ts
@@ -1,3 +1,6 @@
 'use server';
 
-export async function createTopic(){}
+export async function createTopic(formData: FormData) {
+    const name = formData.get('name');
+    const description = formData.get('description')
+}
