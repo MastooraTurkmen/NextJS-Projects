92afb3854219ae8a9c7e28205835c71f426fd9a4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-22 20:45:56 +0430

    show topic and save file

--- discuss-app/src/components/topics/topic-create-form.tsx
+++ discuss-app/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Input,
     Button,
@@ -6,24 +8,46 @@ import {
     PopoverTrigger, 
     PopoverContent
 } from '@nextui-org/react'
+import * as actions from '@/actions'
+import { useFormState } from 'react-dom'
 
 const TopicCreateForm = () => {
+    const [formState, action] = useFormState(actions.createTopic, {
+        errors: {}
+    })
     return (
         <Popover placement='left'>
             <PopoverTrigger>
                 <Button color='primary'>Create a Topic</Button>
             </PopoverTrigger>
             <PopoverContent>
-                <form>
+                <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Topic</h3>
-                        <Input name='name' label='Name' labelPlacement='outside' placeholder='Name' />
+                        <Input
+                            name='name'
+                            label='Name'
+                            labelPlacement='outside'
+                            placeholder='Name'
+                            isInvalid={!!formState.errors.name}
+                            errorMessage={formState.errors.name?.join(', ')}
+                            />
                         <Textarea
                             name='description'
                             label="Description"
                             labelPlacement='outside'
                             placeholder='Describe your topic'
+                            isInvalid={!!formState.errors.description}
+                            errorMessage={formState.errors.description?.join(', ')}
                         />
+
+                        {formState.errors._form ? (
+                            <div className="rouneded p-2 bg-red-200 border border-red-400">
+                                {formState.errors._form?.join(', ')}
+                            </div>
+                        ): null}
+
+
                         <Button type='submit'>Submit</Button>
                     </div>
                 </form>
