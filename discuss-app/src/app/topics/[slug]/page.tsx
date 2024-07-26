e1596497ff8e908cc5fd902f22f799cce1d15df7
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-26 21:02:56 +0430

    discuss page

--- discuss-app/src/app/topics/[slug]/page.tsx
+++ discuss-app/src/app/topics/[slug]/page.tsx
@@ -14,7 +14,7 @@ const TopicShowPage = ({ params }: TopicShowPageProps) => {
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
       </div>
       <div>
-        <PostCreateForm />
+        <PostCreateForm slug={slug} />
       </div>
     </div>
   )
