917b6587b84fb58c126e975ca8a1f37ff2317e2e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-28 16:58:12 +0430

    fetching the big list

--- discuss-app/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss-app/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -3,6 +3,7 @@ import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 
 interface PostShowPageProps {
   params: {
@@ -21,7 +22,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       </Link>
       <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
-      {/* <CommentList comments={comments} /> */}
+      <CommentList fetchData={()=> fetchCommentsByPostId(postId)} />
     </div>
   );
 }
