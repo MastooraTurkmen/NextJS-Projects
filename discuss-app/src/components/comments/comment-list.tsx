19124fb817766bf7bc1a96f724a255669b0c873a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-28 16:56:12 +0430

    fetching the big list

--- discuss-app/src/components/comments/comment-list.tsx
+++ discuss-app/src/components/comments/comment-list.tsx
@@ -1,9 +1,12 @@
 import CommentShow from "@/components/comments/comment-show";
+import type { CommentWithAuth } from "@/db/queries/comments";
 
-interface CommentListProps {}
+interface CommentListProps {
+  fetchData: ()=> Promise<CommentWithAuth[]>
+}
 
-// TODO: Get a list of comments from somewhere
-export default function CommentList({}: CommentListProps) {
+export default async function CommentList({ fetchData }: CommentListProps) {
+  const comments = await fetchData();
   const topLevelComments = comments.filter(
     (comment) => comment.parentId === null
   );
