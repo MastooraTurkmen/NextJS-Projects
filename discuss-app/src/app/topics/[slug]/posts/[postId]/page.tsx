ad0ebeaacbe724196b0aace7d1b27b55ac3e07ac
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-29 09:32:07 +0430

    set up

--- discuss-app/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss-app/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,6 +4,8 @@ import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
+import PostShowLoading from "@/components/posts/post-show-loading";
 
 interface PostShowPageProps {
   params: {
@@ -20,9 +22,11 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading/>}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
-      <CommentList fetchData={()=> fetchCommentsByPostId(postId)} />
+      <CommentList postId={postId} />
     </div>
   );
 }
