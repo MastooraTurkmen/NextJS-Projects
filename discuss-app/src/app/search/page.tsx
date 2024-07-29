1f0bbf4f92efc631e54da24c6743bde1f9c97538
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-29 09:31:48 +0430

    search post

---
+++ discuss-app/src/app/search/page.tsx
@@ -0,0 +1,21 @@
+import PostList from "@/components/posts/post-list";
+import { fetchPostsBySearchTerm } from "@/db/queries/posts";
+import { redirect } from "next/navigation";
+
+interface SearchPageProps {
+    searchParams: {
+        term: string
+    }
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+    const { term } = searchParams;
+    if (!term) {
+        redirect('/')
+    }
+    return (
+        <div>
+            <PostList fetchData={()=> fetchPostsBySearchTerm(term)} />
+        </div>
+  )
+}
