b718e5e796a6c19742f52132127e1d43f2f14af9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-29 09:32:50 +0430

    post show

--- discuss-app/src/components/posts/post-show.tsx
+++ discuss-app/src/components/posts/post-show.tsx
@@ -7,6 +7,7 @@ interface PostShowProps {
 }
 
 export default async function PostShow({ postId }: PostShowProps) {
+  await new Promise(resolve => setTimeout(resolve, 2500))
   const post = await db.post.findFirst({
     where: { id: postId }
   });
