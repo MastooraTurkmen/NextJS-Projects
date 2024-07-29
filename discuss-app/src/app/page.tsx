fb0a7fa2e8da5f1bd84468d9c4e43024047c75aa
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-29 09:31:32 +0430

    post list

--- discuss-app/src/app/page.tsx
+++ discuss-app/src/app/page.tsx
@@ -1,5 +1,7 @@
+import PostList from "@/components/posts/post-list"
 import TopicCreateForm from "@/components/topics/topic-create-form"
 import TopicList from "@/components/topics/topic-list"
+import { fetchTopicPosts } from "@/db/queries/posts"
 import { Divider } from "@nextui-org/react"
 
 export default function Home() {
@@ -7,6 +9,7 @@ export default function Home() {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
+        <PostList fetchData={fetchTopicPosts} />
       </div>
       <div className=" border shadow py-3 px-2">
         <TopicCreateForm />
