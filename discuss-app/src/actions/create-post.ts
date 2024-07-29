e9504179578bc5834104ac2a40c996584365f8b4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-29 09:31:04 +0430

    create post

--- discuss-app/src/actions/create-post.ts
+++ discuss-app/src/actions/create-post.ts
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 
 import { auth } from "@/auth";
 import { db } from "@/db";
@@ -9,81 +9,85 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 
 const createPostSchema = z.object({
-    title: z.string().min(3),
-    content: z.string().min(10)
-})
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
 
 interface CreatePostFormState {
-    errors: {
-        title?: string[],
-        content?: string[],
-        _form?: string[]
-    }
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
 }
 
 export async function createPost(
-    slug: string,
-    formState: CreatePostFormState,
-    formData: FormData
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
 ): Promise<CreatePostFormState> {
-    const result = createPostSchema.safeParse({
-        title: formData.get('title'),
-        content: formData.get('content')
-    })
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
 
-    if (!result.success) {
-        return {
-            errors: result.error.flatten().fieldErrors
-        }
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Cannot find topic"],
+      },
+    };
+  }
+
+  if (!session?.user?.id) {
+    throw new Error("User ID is required");
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Failed to create post"],
+        },
+      };
     }
+  }
 
-    const session = await auth();
-    if (!session || !session.user) {
-        return {
-            errors: { 
-                _form: ['You must be signed in to do this'],
-            }
-        }
-    }
-
-    const topic = await db.topic.findFirst({
-        where: {slug}
-    })
-
-    if (!topic) {
-        return {
-            errors: {
-                _form: ['Cannot find topic'],
-            }
-        }
-    }
-
-    let post: Post;
-    try {
-        post = await db.post.create({
-            data: {
-                title: result.data.title,
-                content: result.data.content,
-                userId: session.user.id,
-                topicId: topic.id
-            }
-        })
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            return {
-                errors: {
-                    _form: [error.message]
-                }
-            }
-        } else {
-            return {
-                errors: {
-                    _form: ['Failed to create post']
-                }
-            }
-        }
-    }
-
-    revalidatePath(paths.topicShow(slug));
-    redirect(paths.postShow(slug, post.id));
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
 }
